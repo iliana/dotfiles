c169f85f2f4a38f3f46032899e6056a46e836100
iliana etaoin <iliana@buttslol.net>
2023-03-12 21:54:20 +0000

    flake: avoid having a system name in the output

--- .config/dotfiles/flake.nix
+++ .config/dotfiles/flake.nix
@@ -1,15 +1,11 @@
 {
   outputs = { ... }: {
-    packages.x86_64-linux.default =
-      let
-        root = ./../..;
-      in
-      with builtins; filterSource
-        (path: type: all (p: path != toString (root + p)) [
-          /.config/dotfiles/flake.nix
-          /git
-          /Library
-        ])
-        root;
+    dotfiles = let root = ./../..; in with builtins; filterSource
+      (path: type: all (p: path != toString (root + p)) [
+        /.config/dotfiles/flake.nix
+        /git
+        /Library
+      ])
+      root;
   };
 }
